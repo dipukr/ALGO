4b1870355f01ed5c7a6fca2e8d0c405822620515
adipukr <dipu.kumar@50hertz.in>
2025-06-18 18:25:03 +0530

    chg

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -2,8 +2,12 @@ package algo;
 
 public class Main {
 	public static void main(final String[] args) {
-		Mat mat = new Mat(3, 3);
-		mat.randomize();
-		System.out.println(mat);
+		Mat m = new Mat(3, 3);
+		m.init(1);
+		//Mat n = new Mat(3, 3);
+		//Mat c = MatUtils.add(m, n);
+		System.out.println(m);
+		//System.out.println(n);
+		//System.out.println(c);
 	}
 }
--- src/main/java/algo/Mat.java
+++ src/main/java/algo/Mat.java
@@ -2,38 +2,37 @@ package algo;
 
 public class Mat {
 
-	private double[][] data;
-	private int rows;
-	private int cols;
+	public double[][] data;
 
 	public Mat(int rows, int cols) {
-		this.rows = rows;
-		this.cols = cols;
 		this.data = new double[rows][cols];
 	}
 
 	public void init(double val) {
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < cols; j++)
+		for (int i = 0; i < rows(); i++)
+			for (int j = 0; j < cols(); j++)
 				data[i][j] = val;
 	}
 
 	public void randomize() {
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < cols; j++)
+		for (int i = 0; i < rows(); i++)
+			for (int j = 0; j < cols(); j++)
 				data[i][j] = Math.random();
 	}
+	
+	public int rows() {return data.length;}
+	public int cols() {return data[0].length;}
 	
 	@Override
 	public String toString() {
 		var text = new StringBuilder();
-		for (int i = 0; i < rows; i++) {
-			System.out.printf("[");
-			for (int j = 0; j < cols; j++) {
-				System.out.printf("%f", data[i][j]);
-				if (j < cols - 1) System.out.printf(" ");
+		for (int i = 0; i < rows(); i++) {
+			text.append("[");
+			for (int j = 0; j < cols(); j++) {
+				text.append(data[i][j]);
+				if (j < cols() - 1) text.append(" ");
 			}
-			System.out.printf("]\n");
+			text.append("]\n");
 		} 
 		return text.toString();
 	}
