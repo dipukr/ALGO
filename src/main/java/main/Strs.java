41683fb43c147b676032aa5ef68c8f4005d4a50a
adipukr <dipu.kumar@50hertz.in>
2025-09-09 18:09:51 +0530

    chg

---
+++ src/main/java/main/Strs.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Strs {
+	public String compressedString(String word) {
+		var result = new StringBuilder();
+		int i = 1, count = 1;
+		char prevChar = word.charAt(0);
+		while (true) {
+			char currChar = word.charAt(i++);
+			if (currChar == prevChar && count < 9) {
+				count++;
+			} else {
+				result.append(count);
+				result.append(prevChar);
+				count = 1;
+			}
+			if (i == word.length()) {
+				result.append(count);
+				result.append(currChar);
+				break;
+			}
+			prevChar = currChar;
+		}
+		return String.valueOf(result);
+	}
+}
